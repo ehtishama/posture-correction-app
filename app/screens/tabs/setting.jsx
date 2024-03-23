3f77d6010ef9f254ab3cf285767350134363d64c
ehtishama <ehtishamhassan9@gmail.com>
2024-03-23 12:02:00 +0000

    Design a minimal settings screen

--- app/screens/tabs/setting.jsx
+++ app/screens/tabs/setting.jsx
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import ScreenLayout from "../screen_layout";
 import typography from "../../styles/typography";
@@ -9,8 +15,8 @@ const SECTIONS = [
   {
     header: "Support",
     items: [
-      { type: "link", label: "Report an Issue", icon: "bug" },
-      { type: "link", label: "Report an Issue", icon: "bug" },
+      { type: "link", label: "Report an Issue", icon: "bug-outline" },
+      { type: "link", label: "Contact us", icon: "email-outline" },
     ],
   },
 ];
@@ -19,31 +25,35 @@ export default function SettingScreen() {
   return (
     <ScreenLayout style={styles.container}>
       <Text style={typography.titleLarge}>Settings</Text>
-      {SECTIONS.map((section) => (
-        <View>
-          <Text style={[typography.bodyBase, styles.section_header]}>
-            {section.header}
-          </Text>
-          {section.items.map((item, idx) => (
-            <>
-              <View style={styles.item}>
-                <MaterialCommunityIcons name={item.icon} size={24} />
-                <Text style={typography.titleBase}>{item.label}</Text>
-                {item.type === "link" && (
-                  <MaterialCommunityIcons
-                    name="chevron-right"
-                    size={24}
-                    style={{ marginLeft: "auto" }}
-                  />
+      <ScrollView style={styles.sections_list}>
+        {SECTIONS.map((section) => (
+          <View>
+            <Text style={[typography.bodyBase, styles.section_header]}>
+              {section.header}
+            </Text>
+            {section.items.map((item, idx) => (
+              <>
+                <TouchableOpacity style={styles.item}>
+                  <MaterialCommunityIcons name={item.icon} size={24} />
+                  <Text style={typography.titleBase}>{item.label}</Text>
+                  {item.type === "link" && (
+                    <MaterialCommunityIcons
+                      name="chevron-right"
+                      size={24}
+                      style={{ marginLeft: "auto" }}
+                    />
+                  )}
+                </TouchableOpacity>
+                {idx + 1 < section.items.length && (
+                  <View style={styles.divider} />
                 )}
-              </View>
-              {idx + 1 < section.items.length && (
-                <View style={styles.divider} />
-              )}
-            </>
-          ))}
-        </View>
-      ))}
+              </>
+            ))}
+          </View>
+        ))}
+
+        <Text>App version 1.0.0</Text>
+      </ScrollView>
     </ScreenLayout>
   );
 }
@@ -53,6 +63,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 16,
+    gap: 8,
+  },
+  sections_list: {
     gap: 8,
   },
   item: {
