244d139bd4643b4b2bb3bf5575de23c2a575cd8e
ehtishama <ehtishamhassan9@gmail.com>
2024-03-26 17:04:11 +0000

    Integrate formspark to receive form submissions

--- app/screens/feeback_screen.jsx
+++ app/screens/feeback_screen.jsx
@@ -1,12 +1,33 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import React from "react";
+import { Alert, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
 import ScreenLayout from "./screen_layout";
 import Button from "../components/button";
 import { colors } from "../styles/colors";
 import Spacer from "../components/spacer";
 import typography from "../styles/typography";
+import { useFormspark } from "@formspark/use-formspark";
+
+const FORMSPARK_FORM_ID = "fGCMTHwW7";
 
 export default function FeedbackScreen() {
+  const [submit, submitting] = useFormspark({
+    formId: FORMSPARK_FORM_ID,
+  });
+
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const handleSend = async () => {
+    if (email && message) {
+      await submit({ email, message });
+      Alert.alert(
+        "Message Sent",
+        "Thanks for valueable feedback. We'll be looking into it soon, and get back to you",
+        [{ text: "OK" }]
+      );
+    }
+  };
+
   return (
     <ScreenLayout style={styles.container}>
       <Text style={typography.titleLarge}>Reach out now!</Text>
@@ -16,6 +37,8 @@ export default function FeedbackScreen() {
         placeholder="Email"
         style={styles.text_input}
         keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
       />
       <TextInput
         placeholder="Write your message here"
@@ -23,9 +46,11 @@ export default function FeedbackScreen() {
         multiline
         numberOfLines={6}
         textAlignVertical="top"
+        value={message}
+        onChangeText={setMessage}
       />
       <Spacer />
-      <Button text="Send" primary />
+      <Button text="Send" primary onPress={handleSend} />
     </ScreenLayout>
   );
 }
